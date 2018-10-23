86f81b2419c4ce65e22f4c37e932ef2896f59f5b
pablissimo77 <pablissimo77@gmail.com>
2018-10-23 12:34:57 +0300

    color-font-in-restructuredtext
    Nikola version: 8.0.1

--- conf.py
+++ conf.py
@@ -1127,18 +1127,18 @@ MARKDOWN_EXTENSIONS = ['extra']
 # """
 
 # Show link to source for the posts?
-# SHOW_SOURCELINK = True
+SHOW_SOURCELINK = False
 # Copy the source files for your pages?
 # Setting it to False implies SHOW_SOURCELINK = False
 # COPY_SOURCES = True
 
 # Modify the number of Post per Index Page
 # Defaults to 10
-# INDEX_DISPLAY_POST_COUNT = 10
+INDEX_DISPLAY_POST_COUNT = 10
 
 # By default, Nikola generates RSS files for the website and for tags, and
 # links to it.  Set this to False to disable everything RSS-related.
-# GENERATE_RSS = True
+GENERATE_RSS = True
 
 # By default, Nikola does not generates Atom files for indexes and links to
 # them. Generate Atom for tags by setting TAG_PAGES_ARE_INDEXES to True.
