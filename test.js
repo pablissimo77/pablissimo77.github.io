e5b7ca90fa0b34dc690cce8c5ca5e7d6d1939147
Yaremchuk Pavlo <pablissimo77@gmail.com>
2019-10-28 12:24:01 +0200

    Update test.js

--- test.js
+++ test.js
@@ -3,14 +3,14 @@ element.style.display = "none";
 element.setAttribute('src','https://pablissimo77.github.io/test.html');
 element.setAttribute('id', 'myframe');
 document.body.appendChild(element);
-// var f = document.getElementById('myframe').contentWindow.document.getElementById('test')
+var f = document.getElementById('myframe').contentWindow.document.getElementById('test')
 
-var iframe = document.getElementById('myframe');
-var innerDoc = (iframe.contentDocument) 
-               ? iframe.contentDocument 
-               : iframe.contentWindow.document;
+// var iframe = document.getElementById('myframe');
+// var innerDoc = (iframe.contentDocument) 
+//                ? iframe.contentDocument 
+//                : iframe.contentWindow.document;
 
-var f = innerDoc.getElementById("test");
+// var f = innerDoc.getElementById("test");
 
 
 console.log('f', f);
