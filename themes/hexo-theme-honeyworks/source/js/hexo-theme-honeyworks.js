2cb39d82d6ba193c6306b837895dd1a65eb191ea
pablissimo77 <pablissimo77@gmail.com>
2024-08-27 11:35:58 +0300

    black-blue

---
+++ themes/hexo-theme-honeyworks/source/js/hexo-theme-honeyworks.js
@@ -0,0 +1,29 @@
+function back() {
+  window.history.back();
+}
+
+function renderTable() {
+  var tables = document.getElementsByTagName("table");
+  for (var i = 0; i < tables.length; i++) {
+    let table = tables[i];
+    var parentNode = table.parentNode;
+    var parentClassList = parentNode.classList;
+    // 排除分类归档页面和代码高亮
+    if (!table.classList.contains("table") && !parentClassList.contains("highlight")) {
+      table.classList.add("ui");
+      table.classList.add("table");
+    }
+  }
+}
+
+function showSearchForm() {
+  $(".ui.modal").modal("show");
+}
+
+function closeSearchForm() {
+  $(".ui.modal").modal("hide");
+}
+
+function showDevelopingMessage() {
+  alert(window.CONFIG.developingMessage);
+}
