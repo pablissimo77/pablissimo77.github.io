8c964d8a3bcca44f3fbcfb351ab09a8d94c310e2
pablissimo77 <pablissimo77@gmail.com>
2024-08-27 15:30:01 +0300

    maupassant-hexo

--- themes/hexo-theme-ylion/scripts/filter/emptycode.filter.js
+++
@@ -1,6 +0,0 @@
-function filterEmptyCodeLine (data) {
-  data.content = data.content.replace(/<div class="line">\s*<\/div>/mg, '');
-  return data;
-}
-
-hexo.extend.filter.register('after_post_render', filterEmptyCodeLine);
