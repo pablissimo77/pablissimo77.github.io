7ea3c640f2e222087cdca6a7cffdc969d795be84
pablissimo77 <pablissimo77@gmail.com>
2024-08-27 10:55:51 +0300

    hexo-theme-phase

--- themes/hexo-theme-ylion/scripts/helper/getCurPosts.js
+++
@@ -1,4 +0,0 @@
-function getCurPosts(sortedPosts, config, page) {
-    return sortedPosts.slice((page.current - 1) * config.per_page, page.current * config.per_page);
-}
-hexo.extend.helper.register("getCurPosts",getCurPosts);
